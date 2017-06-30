c57c7ccb7a89dffad1d37a36600729ad41a525d1
Victor Maia <srvictormaia@gmail.com>
2017-06-30 03:01:21 -0300

    Fix options param

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var request = require('xhr-request')
 
 module.exports = function (url, options) {
   return new Promise(function (resolve, reject) {
-    request(url, {}, function (err, data) {
+    request(url, options, function (err, data) {
       if (err) reject(err);
       else resolve(data);
     });
